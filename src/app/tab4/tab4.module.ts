41955e07341872b07a40752b830677d3c29d7aeb
cheo2322 <barcecheo2319@gmail.com>
2019-12-25 21:21:24 -0500

    Patalla resumen

---
+++ src/app/tab4/tab4.module.ts
@@ -0,0 +1,19 @@
+import { IonicModule } from '@ionic/angular';
+import { RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Tab4Page } from './tab4.page';
+
+@NgModule({
+  imports: [
+    IonicModule,
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forChild([{ path: '', component: Tab4Page }])
+  ],
+  declarations: [Tab4Page]
+  // ,  providers:[BarcodeScanner]
+})
+export class Tab4PageModule {}
---
+++ src/app/tab4/tab4.page.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-tab4',
+  templateUrl: 'tab4.page.html',
+  styleUrls: ['tab4.page.scss']
+})
+export class Tab4Page {
+}
