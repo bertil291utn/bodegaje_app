678a5c8a44e356d8e4bb338d50185c356fcbf9f7
cheo2322 <barcecheo2319@gmail.com>
2019-12-25 20:52:49 -0500

    new changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,11 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  }
+  },
+  /*{
+    path: 'tab4',
+    loadChildren: () => import('./tab4/tab4.module').then( m => m.Tab4PageModule)
+  }*/
 ];
 @NgModule({
   imports: [
